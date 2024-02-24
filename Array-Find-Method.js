885bc0bf7da4612b602348ce3166e1e6e52d676f
Rajesh Korlapati <77844866+rajesh2150@users.noreply.github.com>
2024-02-24 13:06:22 +0530

    Update Array-Find-Method.js
    Update Array-Find-Method.js

--- Array-Find-Method.js
+++ Array-Find-Method.js
@@ -1,3 +1,3 @@
-let cart=[19,293,48,39]
+let cart=[19,293,48,39] //.find -- based on condition gives first satisfying value or false gives undefined ✅
 const find =  cart.find((val)=>val>10)
 console.log(find)
