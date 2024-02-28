13037d3eaf665ed96df6e5ab3c957bc18d65cf4e
Rajesh Korlapati <77844866+rajesh2150@users.noreply.github.com>
2024-02-28 16:14:05 +0530

    Create Storage-SessionStorage.js

---
+++ Storage-SessionStorage.js
@@ -0,0 +1,17 @@
+//storage types
+//sessionStorage(key,value) remove/loose all data after refresh/close tab
+
+sessionStorage.setItem("name","Ravi") // for set data to sessionStorage 
+
+const name =sessionStorage.getItem("name") // for get data from sessionStorage
+console.log(name)
+
+//cart
+const cartItems={name:"Apple15Pro",price:100,rating:4}
+
+sessionStorage.setItem("cartValue",JSON.stringify(cartItems)) // without stringify we can get error 
+const cart = sessionStorage.getItem("cartValue")
+console.log(cart)
+const cartObject = sessionStorage.getItem("cartValue")
+const valuesOfCart =JSON.parse(cartObject)
+console.log(valuesOfCart.name,valuesOfCart.price,valuesOfCart.rating+"⭐")
