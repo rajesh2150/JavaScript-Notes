e5db2ca7a47e1f5fcd70ee27efc6515ec8637887
Rajesh Korlapati <77844866+rajesh2150@users.noreply.github.com>
2024-02-28 15:53:28 +0530

    Create setInterval.js

---
+++ setInterval.js
@@ -0,0 +1,4 @@
+//setInterval(cal,time) repeatedly calls a function or executes a code 	snippet, with a fixed time delay between each call.
+let count = 0
+setInterval(() => {count++; console.log("This Is SetInterVal", count)}
+, 3000)
